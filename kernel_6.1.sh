20f77ca48edb2775fa099dd043d33f578c32ce18
杨黄林 <yhl452493373@gmail.com>
2024-06-25 21:26:49 +0800

    Create kernel_6.1.sh

---
+++ kernel_6.1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#run this in openwrt workdir
+
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+git clone --depth 1 https://github.com/coolsnowwolf/lede.git ../lede
+
+cp ../lede/include/kernel-6.1 include/kernel-6.1 
+
+rm -rf target/linux/generic/backport-6.1
+cp -r ../lede/target/linux/generic/backport-6.1 target/linux/generic/backport-6.1
+
+rm -rf target/linux/generic/hack-6.1
+cp -r ../lede/target/linux/generic/hack-6.1 target/linux/generic/hack-6.1
+
+rm -rf target/linux/generic/pending-6.1
+cp -r ../lede/target/linux/generic/pending-6.1 target/linux/generic/pending-6.1
+
+mkdir -p target/linux/x86/patches-6.1
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/patches-6.1/100-fix_cs5535_clockevt.patch -o target/linux/x86/patches-6.1/100-fix_cs5535_clockevt.patch
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/patches-6.1/103-pcengines_apu6_platform.patch -o target/linux/x86/patches-6.1/103-pcengines_apu6_platform.patch
+
+cp ../lede/include/kernel-6.1 include/kernel-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/generic/config-6.1 -o target/linux/generic/config-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/64/config-6.1 -o target/linux/x86/64/config-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/config-6.1 -o target/linux/x86/config-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/generic/config-6.1 -o target/linux/x86/generic/config-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/geode/config-6.1 -o target/linux/x86/geode/config-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/legacy/config-6.1 -o target/linux/x86/legacy/config-6.1
+
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/485afd4b1107d38dff94bca106ecf260982dee35/target/linux/x86/Makefile -o target/linux/x86/Makefile
