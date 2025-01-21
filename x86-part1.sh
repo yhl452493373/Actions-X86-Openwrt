75e8d697a8a4a10d261762b3ab1e969f92a827e2
杨黄林 <yhl452493373@gmail.com>
2025-01-21 18:14:56 +0800

    Update x86-part1.sh

--- x86-part1.sh
+++ x86-part1.sh
@@ -33,10 +33,6 @@ git clone https://github.com/yhl452493373/luci-theme-argon.git package/luci-them
 # infinityfreedom-ng主题
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
 
-# kucat主题
-git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
-git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
-
 # 高级设置
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 
